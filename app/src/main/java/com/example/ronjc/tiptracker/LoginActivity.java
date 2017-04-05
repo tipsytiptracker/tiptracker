ebbbe7788a62422e093bedceb606a0b1d1c14ab8
Alex <pomponioalex@gmail.com>
2017-04-05 15:22:50 -0400

    Test Branch Commit

--- app/src/main/java/com/example/ronjc/tiptracker/LoginActivity.java
+++ app/src/main/java/com/example/ronjc/tiptracker/LoginActivity.java
@@ -49,6 +49,7 @@ import static android.Manifest.permission.READ_CONTACTS;
 /**
  * A login screen that offers login via email/password.
  */
+//Test comment
 public class LoginActivity extends AppCompatActivity {
     private AutoCompleteTextView mEmailView;
     private EditText mPasswordView;
