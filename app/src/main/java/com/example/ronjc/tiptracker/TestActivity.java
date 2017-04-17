b36d6ab191803909674e44e8e51b5e5c0572f2e3
Alex <pomponioalex@gmail.com>
2017-04-17 01:23:34 -0400

    Add OCR util

---
+++ app/src/main/java/com/example/ronjc/tiptracker/TestActivity.java
@@ -0,0 +1,53 @@
+package com.example.ronjc.tiptracker;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.ronjc.tiptracker.utils.Camera;
+import com.example.ronjc.tiptracker.utils.OCR;
+
+public class TestActivity extends AppCompatActivity {
+    Camera mCam;
+    OCR mOCR;
+    final int REQUEST_TAKE_PHOTO = 1;
+    String path;
+    TextView textView;
+    ImageView imageView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+        textView = (TextView) findViewById(R.id.textView);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        mCam = new Camera(this);
+
+
+    }
+
+    public void takePic(View view) {
+        mCam.takePicture();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+            path = mCam.getPath();
+            imageView.setImageURI(mCam.getUri());
+
+        }
+    }
+
+    public void goOCR(View view) {
+        mOCR = new OCR(this, path);
+        textView.setText(mOCR.getTotal());
+    }
+}
---
+++ app/src/main/java/com/example/ronjc/tiptracker/utils/OCR.java
@@ -0,0 +1,59 @@
+package com.example.ronjc.tiptracker.utils;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.SparseArray;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+
+import java.util.List;
+
+/**
+ * Created by Alex on 4/15/2017.
+ */
+
+public class OCR {
+
+    private TextRecognizer textRecognizer;
+    private Bitmap bitmap;
+    private SparseArray<TextBlock> textBlocks;
+
+    public OCR(Context context, String imagePath) {
+        textRecognizer = new TextRecognizer.Builder(context).build();
+        bitmap = BitmapFactory.decodeFile(imagePath);
+        Frame imageFrame = new Frame.Builder()
+                .setBitmap(bitmap)
+                .build();
+
+        textBlocks = textRecognizer.detect(imageFrame);
+    }
+
+    public String getTotal() {
+        String total = "";
+        List<? extends com.google.android.gms.vision.text.Text> lineList;
+        List<? extends com.google.android.gms.vision.text.Text> wordList;
+        outerloop:
+        for (int i = 0; i < textBlocks.size(); i++) {
+            TextBlock textBlock = textBlocks.get(textBlocks.keyAt(i));
+            lineList = textBlock.getComponents();
+            for (int j = 0; j < lineList.size(); j++) {
+                wordList = lineList.get(j).getComponents();
+                for (int k = 0; k < wordList.size(); k++) {
+                    if (wordList.get(k).getValue().equalsIgnoreCase("total")) {
+                        if (wordList.get(k + 1).getValue().equals("$") ||
+                                wordList.get(k + 1).getValue().equalsIgnoreCase("s") )
+                            total = wordList.get(k + 2).getValue();
+                         else
+                            total = wordList.get(k + 1).getValue();
+                        break outerloop;
+                    }
+                }
+            }
+
+        }
+        return total;
+    }
+}
