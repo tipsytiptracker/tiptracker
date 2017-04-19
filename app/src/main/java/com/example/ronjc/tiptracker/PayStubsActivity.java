8a4bf1de4019785b37818db0ef489f03fdac46fa
Arthur Rozenberg <rozenarthur@gmail.com>
2017-04-19 01:17:54 -0400

    show all dates of all the users paystubs with the error fix of getting info from db

--- app/src/main/java/com/example/ronjc/tiptracker/PayStubsActivity.java
+++ app/src/main/java/com/example/ronjc/tiptracker/PayStubsActivity.java
@@ -1,9 +1,12 @@
 package com.example.ronjc.tiptracker;
 
 import android.graphics.Typeface;
+import android.icu.text.DateFormat;
+import android.icu.text.SimpleDateFormat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -15,8 +18,14 @@ import com.example.ronjc.tiptracker.model.PayStub;
 import com.example.ronjc.tiptracker.utils.FontManager;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -117,8 +126,67 @@ public class PayStubsActivity extends AppCompatActivity {
                 final AlertDialog.Builder mBuilder2 = new AlertDialog.Builder(PayStubsActivity.this);
                 View mView2 = getLayoutInflater().inflate(R.layout.fragment_view_all_paystubs, null);
 
+                final TextView amount_tv = (TextView)mView2.findViewById(R.id.paystubs_am);
+                final TextView desc_tv = (TextView)mView2.findViewById(R.id.paystubs_desc);
+                final TextView date_tv = (TextView)mView2.findViewById(R.id.paystubs_date);
+
+                Typeface iconFont3 = FontManager.getTypeface(getApplicationContext(), FontManager.BITTER);
+                FontManager.markAsIconContainer(mView2.findViewById(R.id.paystubs_am), iconFont3);
+                FontManager.markAsIconContainer(mView2.findViewById(R.id.paystubs_title), iconFont3);
+                FontManager.markAsIconContainer(mView2.findViewById(R.id.paystubs_desc), iconFont3);
+                FontManager.markAsIconContainer(mView2.findViewById(R.id.paystubs_date), iconFont3);
+
                 mBuilder2.setView(mView2);
                 final AlertDialog dialog = mBuilder2.create();
+
+                myRef.child("users").child(user.getUid()).child("paystubs").orderByChild("datePosted").addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        //gets all of the paystubs of the user
+                        String amount_tit = "Amount\n";
+                        String desc_tit = "Description\n";
+                        String date_tit = "Date Added\n";
+
+                        ArrayList<String> list1 = new ArrayList<String>();
+                        ArrayList<String> list2 = new ArrayList<String>();
+                        ArrayList<String> list3 = new ArrayList<String>();
+
+                        for (DataSnapshot child: dataSnapshot.getChildren()) {
+
+                            PayStub users = child.getValue(PayStub.class);
+
+                            String get_amount = users.getAmount() + "\n";
+                            String get_desc = users.getDescription() + "\n";
+                            long get_date = users.getDatePosted();
+
+                            DateFormat df = new SimpleDateFormat("MM/dd/yy");
+                            String gd = df.format(get_date) + "\n";
+
+                            list1.add(get_amount);
+                            list2.add(get_desc);
+                            list3.add(gd);
+
+                        }
+
+                        Collections.reverse(list1); //reverses arraylist by date so it prints latest first
+                        Collections.reverse(list2);
+                        Collections.reverse(list3);
+
+                        String amounts = TextUtils.join("", list1); //transfers arraylist string
+                        String descs = TextUtils.join("", list2);
+                        String dates = TextUtils.join("", list3);
+
+                        amount_tv.setText(amount_tit + amounts);
+                        desc_tv.setText(desc_tit + descs);
+                        date_tv.setText(date_tit + dates);
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
 
                 dialog.show();
             }
