1f55267bd1ffc17b23509fcf97313ca7e60848d3
Alex <pomponioalex@gmail.com>
2017-04-21 18:00:26 -0400

    Add OCR support for reading totals across columns

--- app/src/main/java/com/example/ronjc/tiptracker/utils/OCR.java
+++ app/src/main/java/com/example/ronjc/tiptracker/utils/OCR.java
@@ -2,7 +2,7 @@ package com.example.ronjc.tiptracker.utils;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.graphics.Rect;
 import android.util.SparseArray;
 
 import com.google.android.gms.vision.Frame;
@@ -29,24 +29,52 @@ public class OCR {
         textBlocks = textRecognizer.detect(imageFrame);
     }
 
+
     public String getTotal() {
         String total = "";
+        TextBlock paragraph;
         List<? extends com.google.android.gms.vision.text.Text> lineList;
         List<? extends com.google.android.gms.vision.text.Text> wordList;
-        outerloop:
+        Rect lineRect = new Rect(0, 0, 0, 0);
+        Rect wordRect = new Rect(0, 0,0 , 0);
+
+        firstloop:
         for (int i = 0; i < textBlocks.size(); i++) {
-            TextBlock textBlock = textBlocks.get(textBlocks.keyAt(i));
-            lineList = textBlock.getComponents();
+            paragraph = textBlocks.get(textBlocks.keyAt(i));
+            lineList = paragraph.getComponents();
             for (int j = 0; j < lineList.size(); j++) {
                 wordList = lineList.get(j).getComponents();
                 for (int k = 0; k < wordList.size(); k++) {
-                    if (wordList.get(k).getValue().equalsIgnoreCase("total")) {
-                        if (wordList.get(k + 1).getValue().equals("$") ||
-                                wordList.get(k + 1).getValue().equalsIgnoreCase("s") )
-                            total = wordList.get(k + 2).getValue();
-                         else
-                            total = wordList.get(k + 1).getValue();
-                        break outerloop;
+                    if (wordList.get(k).getValue().equalsIgnoreCase("total") || wordList.get(k).getValue().equalsIgnoreCase("total:")) {
+                        lineRect = lineList.get(j).getBoundingBox();
+                        wordRect = wordList.get(k).getBoundingBox();
+                        if (k + 1 < wordList.size()) {
+                            if (wordList.get(k + 1).getValue().equals("$") ||
+                                    wordList.get(k + 1).getValue().equals("s")) {
+                                total = wordList.get(k + 2).getValue();
+                            } else
+                                total = wordList.get(k + 1).getValue();
+                            break firstloop;
+                        }
+                    }
+                }
+            }
+        }
+
+        secondloop:
+        for (int i = 0; i < textBlocks.size(); i++) {
+            paragraph = textBlocks.get(textBlocks.keyAt(i));
+            lineList = paragraph.getComponents();
+
+            for (int j = 0; j < lineList.size(); j++) {
+                wordList = lineList.get(j).getComponents();
+
+                for (int k = 0; k < wordList.size(); k++) {
+                    if (wordList.get(k).getBoundingBox().top > wordRect.top - 15 &&
+                            wordList.get(k).getBoundingBox().top < wordRect.top + 15 &&
+                            !lineList.get(j).getBoundingBox().equals(lineRect)) {
+                        total = wordList.get(k).getValue();
+                        break secondloop;
                     }
                 }
             }
