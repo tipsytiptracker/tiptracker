fca037ff6521327f5cb14aa6d5344ea945af13dd
geneh96 <geneh96@gmail.com>
2017-04-30 22:12:27 -0400

    Add layout to paystubgallery

--- app/src/main/java/com/example/ronjc/tiptracker/PaystubGallery.java
+++ app/src/main/java/com/example/ronjc/tiptracker/PaystubGallery.java
@@ -2,6 +2,10 @@ package com.example.ronjc.tiptracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Gallery;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class PaystubGallery extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class PaystubGallery extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paystub_gallery);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.tiptrackerlogo3);
+        getSupportActionBar().setTitle("");
+        ButterKnife.bind(this);
+
+
+
     }
 }
